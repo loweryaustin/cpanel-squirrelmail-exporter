e5a2980e4c4b108b6fe9aed242c87558f327712e
Austin Lowery <loweryaustin@gmail.com>
2018-12-25 23:43:50 +0000

    Start building out export by email account.

--- export.pl
+++ export.pl
@@ -2,6 +2,7 @@
 use 5.010;
 use strict;
 use warnings;
+use IO::Handle;
 use Time::Piece;
 use Data::Dumper;
 use Fcntl ':mode';
@@ -13,6 +14,11 @@ use File::Path qw(make_path);
 use Getopt::Long qw(GetOptions);
 use Digest::MD5 qw(md5 md5_hex);
 Getopt::Long::Configure qw(gnu_getopt);
+
+use lib '/usr/local/cpanel';
+
+use Cpanel::PwCache                      ();
+use Cpanel::Domain::Owner
 
 ## Options
 my $domain;
@@ -29,4 +35,12 @@ GetOptions(
 
 if ( scalar grep($_, $domain, $emailAcct, $cpanelAcct) > 1 ) { die ("The --domain, --email-acct, and --cpanel-acct options are mutually exclusive. Re-read the documentation and try again.\n"); };
 
+if (! $emailAcct eq ''){
+	my @emailParts = split /@/,$emailAcct;
+	my $emailUser = $emailParts[0];
+	my $emailDomain = $emailParts[1];
+	my $cpanelAcct = Cpanel::Domain::Owner::get_owner_or_die($emailDomain);
+        my $homedir = Cpanel::PwCache::gethomedir($cpanelAcct);
 
+}
+	
